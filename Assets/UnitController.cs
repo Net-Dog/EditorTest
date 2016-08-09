7e289490d9889760ce5d5d592996d6485796a65b
sem <ficher12345@mail.ru>
2016-08-09 11:47:45 +0300

    UPD: Add new component

---
+++ Assets/UnitController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitController : MonoBehaviour {
+
+    [System.Serializable]
+    public class HeadPoint
+    {
+        public float HP = 300f;
+    }
+    [System.Serializable]
+    public class Speed
+    {
+        public float speed = 5f;
+    }
+    public HeadPoint Hp;
+    public Speed speed;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/UnitEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class UnitEditor : EditorWindow {
+
+    public void OnGUI() {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
